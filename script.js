f38fce7181a3041bcdc34b3685090d1608b8c7a6
Alex Funk <alexdagreat04@gmail.com>
2024-09-17 17:50:38 -0400

    Added basic styling and more interactivity
    Need to replace prompt boxes for player names with a dialog boxes
    Need to add a reset button which will also reprompt for names

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ const Gameboard = (function() {
                        [" ", " ", " "]];
 
 
-    function displayBoard() {
-        console.log(`${gameState[0][0]} | ${gameState[0][1]} | ${gameState[0][2]}\n---------\n${gameState[1][0]} | ${gameState[1][1]} | ${gameState[1][2]}\n---------\n${gameState[2][0]} | ${gameState[2][1]} | ${gameState[2][2]}`);
-    }
-
-
     function updateBoard(symbol, pos1, pos2) {
         if ((0 > pos1 || pos1 > 2) || (0 > pos2 || pos2 > 2)) {
             // Spot out of range
@@ -69,8 +64,9 @@ const Gameboard = (function() {
         return !checkTie() && !checkWin();
     }
 
-    return {displayBoard, updateBoard, checkWin, checkTie, checkContinue};
+    return {updateBoard, checkWin, checkTie, checkContinue};
 })();
+
 
 const Formatter = (function() {
     function setSymbol(target, symbol) {
@@ -81,33 +77,30 @@ const Formatter = (function() {
     return {setSymbol};
 })();
 
+
 const Player = function(symbol, name) {
     return {symbol, name};
 }
 
-const player1 = Player("x", "Alex");
-const player2 = Player("o", "Christian");
-
-const dialogBox = document.querySelector("dialog");
-const dialogBoxP = document.querySelector("dialog div#playerWinText");
-const dialogBoxClose = document.querySelector("dialog button");
-
-let currentPlayer = player2;
 
 function playRound(evt) {
-    currentPlayer = currentPlayer === player1 ? player2 : player1;
     const currentDiv = document.elementFromPoint(evt.clientX, evt.clientY);
     let classList = currentDiv.className;
 
-    if (classList.includes("cell")) {
-        Formatter.setSymbol(currentDiv, currentPlayer.symbol);
-        Gameboard.updateBoard(currentPlayer.symbol, parseInt(currentDiv.getAttribute("pos1")), parseInt(currentDiv.getAttribute("pos2")));
-    } else {
+
+    if (!classList.includes("cell")) {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
+    } else if (!Gameboard.updateBoard(currentPlayer.symbol, parseInt(currentDiv.getAttribute("pos1")), parseInt(currentDiv.getAttribute("pos2")))) {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    } else {
+        Formatter.setSymbol(currentDiv, currentPlayer.symbol);
+        currentDiv.setAttribute("class", "cell");
     }
+
 
     if (!Gameboard.checkContinue()) {
         document.removeEventListener("click", playRound);
+        document.removeEventListener("mouseover", displayGhostOnHover);
 
         if (Gameboard.checkWin()) {
             dialogBoxP.textContent = `${currentPlayer.name} Won`;
@@ -116,7 +109,39 @@ function playRound(evt) {
         }
         dialogBox.showModal();
     }
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
 }
+
+
+function displayGhostOnHover(evt) {
+    const currentDiv = document.elementFromPoint(evt.clientX, evt.clientY);
+    let classText = currentDiv.className;
+    function onMouseLeave() {
+        let classText = currentDiv.className;
+        if (classText.includes("empty")) {
+            currentDiv.textContent = "";
+        }
+
+        currentDiv.removeEventListener("mouseover", displayGhostOnHover);
+        currentDiv.removeEventListener("mouseout", onMouseLeave);
+    }
+
+    if (classText.includes("empty")) {
+        currentDiv.textContent = currentPlayer.symbol;
+        currentDiv.addEventListener("mouseout", onMouseLeave);
+    }
+}
+
+const player1 = Player("X", prompt("Enter player one's(x) name"));
+const player2 = Player("O", prompt("Enter player two'2(x) name"));
+
+const dialogBox = document.querySelector("dialog");
+const dialogBoxP = document.querySelector("dialog div#playerWinText");
+const dialogBoxClose = document.querySelector("dialog button");
+
+let currentPlayer = player1;
+
 
 document.addEventListener("click", playRound);
 dialogBoxClose.addEventListener("click", () => dialogBox.close());
+document.addEventListener("mouseover", displayGhostOnHover);
